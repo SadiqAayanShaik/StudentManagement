49ed1164e2a048624b853e3e31a68ecd9aff4149
aayan <aayan@gmail.com>
2022-09-04 08:55:31 +0530

    first commit

---
+++ src/main/java/com/Student/service/impl/StudentServiceImpl.java
@@ -0,0 +1,47 @@
+package com.Student.service.impl;
+
+import com.Student.entity.Student;
+import com.Student.repository.StudentRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentServiceImpl implements StudentService{
+
+    private StudentRepository studentRepository;
+
+    public StudentServiceImpl(StudentRepository studentRepository) {
+        super();
+        this.studentRepository = studentRepository;
+    }
+
+    @Override
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+
+    @Override
+    public Student saveStudent(Student student) {
+
+        return studentRepository.save(student);
+    }
+
+    @Override
+    public Student getStudentById(Long id) {
+
+
+        return studentRepository.findById(id).get();
+    }
+
+    @Override
+    public Student updateStudent(Student student) {
+
+        return studentRepository.save(student);
+    }
+
+    @Override
+    public void deleteStudentById(Long id) {
+         studentRepository.deleteById(id);
+    }
+}
